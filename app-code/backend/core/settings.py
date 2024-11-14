46309a20591dff6125080fb8d4ff869d8b571e34
Muhammad Rashid <rashiddaha80@gmail.com>
2024-11-14 10:51:17 +0500

    django setup

--- app-code/backend/core/settings.py
+++ app-code/backend/core/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/5.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.0/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -77,12 +77,25 @@ WSGI_APPLICATION = 'core.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.getenv('POSTGRES_DB', 'mydatabase'),
+        'USER': os.getenv('POSTGRES_USERNAME', 'admin'),
+        'PASSWORD': os.getenv('POSTGRES_PASSWORD', 'password123'),
+        'HOST': os.getenv('POSTGRES_HOST', 'postgres-svc'),
+        'PORT': '5432',
     }
 }
+
 
 
 # Password validation
