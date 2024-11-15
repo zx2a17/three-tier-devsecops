06ca58d3759719aff0d3fa97ce18fa3855a742a3
Muhammad Rashid <rashiddaha80@gmail.com>
2024-11-15 19:07:15 +0500

    changes

---
+++ app-code/backend/scripts/boot.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+set -e
+# Apply migrations
+# python manage.py makemigrations --noinput
+python manage.py migrate
+python manage.py collectstatic --noinput
+python manage.py runserver 0.0.0.0:8000
+
+
+
